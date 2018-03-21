9bcac12223b46ebaf21fb1f134aa414ec49468b8
JakeMott <37472591+JakeMott@users.noreply.github.com>
2018-03-21 12:42:51 +1100

    changed child class to be without parents

--- src/Child.java
+++ src/Child.java
@@ -4,17 +4,12 @@ public class Child extends Profile {
 	private Profile parent1;
 	private Profile parent2;
 
-	public Child (String name, String status, int age, Profile parent1, Profile parent2) {
+	public Child (String name, String status, int age) {
 		super(name, status, age);
-		this.parent1 = parent1;
-		this.parent2 = parent2;
-		
 	}
 	
 	public Child (String name, int age, Profile parent1, Profile parent2) {
 		super(name, age);
-		this.parent1 = parent1;
-		this.parent2 = parent2;
 	}
 	
 	
