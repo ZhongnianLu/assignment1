e07b9116f16b1c7301a1cdee528d8b2100ff137d
JakeMott <37472591+JakeMott@users.noreply.github.com>
2018-03-23 10:04:09 +1100

    Update Database.java

--- src/Database.java
+++ src/Database.java
@@ -7,18 +7,18 @@ public class Database {
 	//Profile Bob = new Adult("bob", "Starlet 99", 35);
 	//public void main(String[] args) {
 		
-		Profile Bob = new Adult("bob", "Starlet 99", 35);
-		Profile James = new Adult("James", "BMW 05", 90);
-		Profile Mary = new Adult("Mary", "Holden 03", 60);
-		Profile Lisa = new Adult("Lisa", "Camry 04", 65);
-		Profile Evan = new Child("Evan", "Jaguar 06", 12);
-		Profile Veronica = new Child("Veronica", "Subaru 02", 6);
+		Profile Bob = new Profile("bob", "at work", 35, 1);
+		Profile James = new Profile("James", "eating KFC", 90, 2);
+		Profile Mary = new Profile("Mary", "shopping", 60, 3);
+		Profile Lisa = new Profile("Lisa", "at the tennis", 65, 4);
+		Profile Evan = new Profile("Evan", "reading", 12, 5);
+		Profile Veronica = new Profile("Veronica", "contemplating life", 6, 5);
 		
 		
 	//}
 	
 	public ArrayList<Profile> readProfiles(){		
-		System.out.println("returned profiles");
+		//System.out.println("returned profiles");
 		profiles.add(Bob);
 		profiles.add(James);
 		profiles.add(Mary);
@@ -32,7 +32,7 @@ public class Database {
 		
 	
 	public ArrayList<Connection> readConnections(){		
-		System.out.println("returned conns");
+		//System.out.println("returned conns");
 		conns.add(new Friend_Connection(Bob, Mary));
 		conns.add(new Friend_Connection(Lisa, James));
 		conns.add(new Couple_Connection(James, Mary));
