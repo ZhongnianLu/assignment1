7afd431ed5d94dcfc8090314bf99d87aa0876750
JakeMott <37472591+JakeMott@users.noreply.github.com>
2018-03-23 11:13:32 +1100

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -94,8 +94,8 @@ public class Menu {
 			
 			//System.out.println("Please enter a status update : ");   // enter a status
 			
-			int ID = profiles.get_Plist().size()+1;
-			Profile person = new Profile(name, status, age, ID); //create a profile object
+			//int ID = profiles.get_Plist().size()+1;
+			Profile person = new Profile(name, status, age); //create a profile object
 			//person.setID(profiles.get_Plist().size()+1); //set ID based on number of profiles
 			profiles.addProfile(person);
 			
