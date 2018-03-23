c862e8f6f7d62e22521cb3010aabccc39d945880
JakeMott <37472591+JakeMott@users.noreply.github.com>
2018-03-23 22:46:17 +1100

    Update ProfileManager.java
    commenting

--- src/ProfileManager.java
+++ src/ProfileManager.java
@@ -4,17 +4,24 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /*
- * This class contains all profiles
+ * Author: Jake Mott s3349405
+ * 
+ * This class contains the profile list and methods
+ * to manipulate the list as well as search it and 
+ * other related functions
  * 
  */
 
 public class ProfileManager {
+	/* ArrayList of profile objects to be used to store profiles */
 	ArrayList<Profile> profiles =new ArrayList<Profile>();
 	
+	/* a constructor can take an ArrayList to immediately be filled */
 	public ProfileManager(ArrayList<Profile> profs) {
 		this.profiles = profs;
 	}
 	
+	/* Normal constructor creates empty ArrayList */
 	public ProfileManager() {
 		this.profiles = new ArrayList<Profile>();
 	}
@@ -33,37 +40,47 @@ public class ProfileManager {
 		profiles.remove(del);
 	}
 	
+	/* ArrayList given to fill an empty list or override an existing list */
 	public void importList(ArrayList<Profile> profiles) {
 		int len = profiles.size();
 		
+		/* Iterate over the list to override and delete all entries */
 		for (int i = 0; i < this.get_Plist().size(); i++) { //reset the list
 			this.removeProfile(this.get_Plist().get(i));
 		}
 		
+		/* Iterate over given list and add entries to main list */
 		for (int i = 0; i < len; i++) {
 			this.addProfile(profiles.get(i));
 		}
 		
 	}
 	
-	//checks if a given name is unique among the profiles, return true means it is unique
+	/*checks if a given name is unique among the profiles, return true means it is unique */
 	public boolean uniqueName(String name) {
 		boolean unique = true;
 		name = name.toLowerCase();
 		
-		for (int i = 0; i < this.get_Plist().size(); i++) { //iterates over all profile objects
+		/* Iterates over all profiles to match the given name */
+		for (int i = 0; i < this.get_Plist().size(); i++) { 
 			String str = this.get_Plist().get(i).getName().toLowerCase();
-			if (name.equals(str)) {     //checks if names match
+			
+			/* If names match return a false boolean value */
+			if (name.equals(str)) {    
 				unique = false;
 			}
 		}
 		return unique; 
 	}
 	
+	/* Returns list of adult from the ProfileManager it is used on */
 	public ArrayList<Profile> getAdults() {
 		ArrayList<Profile> tempList = new ArrayList<Profile>();
+		
 		for (int i = 0; i < this.get_Plist().size(); i++) {
 			Profile temp = this.get_Plist().get(i);
+			
+			/* iterate through list and if age is over 16, add to empty list */
 			if (temp.getAge() >= 16) {
 				tempList.add(temp);
 			}
@@ -72,57 +89,65 @@ public class ProfileManager {
 		return tempList;
 	}
 	
+	/* Method to return a selected profile from a given list */
 	public Profile selectProfile(String title) throws IOException{
-		Profile selectProf = null;
+		Profile selectProf = null; //initiate profile object
 		ArrayList<Profile> plist = this.get_Plist();
 		int option;
+		
+		/* Create list of names */
 		ArrayList<String> names = this.listNames();
 		names.add("Exit to Main Menu");
 		
 		do{
-		option = Menu.display_Menu(title, names);
-		if ( option >= 1 && option <= names.size()-1) {
-			selectProf = plist.get(option-1);
-		}
-		break;
+			option = Menu.display_Menu(title, names);
+			if ( option >= 1 && option <= names.size()-1) {
+				selectProf = plist.get(option-1);
+			}
+			break;
 		}while(option != 0);
 		return  selectProf;
 		
 	}
 	
+	/* Method to return list of names of ProgramManager it is used on */
 	public ArrayList<String> listNames() {
 		ArrayList<String> names = new ArrayList<String>();
 		String name;
+		
+		/* Iterate on profile list adding names to list */
 		for (int i = 0; i < this.get_Plist().size(); i++) {
 			name = this.get_Plist().get(i).getName();
 			names.add(name);
 		}				
-		
 		return names;
 	}
 	
+	/* Method to ask and receive profile info from user and create profile object */
 	public Profile askInfo() throws IOException, InputMismatchException {
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println();
+		
+		/* enter name and check if unique */
 		System.out.println("\nPlease enter your name : ");
 		String name = scan.nextLine();
 		if (this.uniqueName(name) != true) throw new IOException("\nError: Name must be unique");
 	
 		
-		System.out.println("\nPlease enter your age : "); //enter age
+		System.out.println("\nPlease enter your age : "); 
 		int age = scan.nextInt();
 		if (age <= 0) throw new IOException("\nError: Age must be nonzero and positive");
 		
 		scan.nextLine();
-		System.out.println("Please enter a status update : ");   // enter a status
+		System.out.println("\nPlease enter a status update : ");   
 		String status = scan.nextLine();
 		
-		Profile person = new Profile(name, status, age); //create a profile object
-		person.setID(get_Plist().size()+1); //set ID based on number of profiles
+		/* create profile object and set ID */
+		Profile person = new Profile(name, status, age); 
+		person.setID(get_Plist().size()+1); 
 		
-		return person;
-				
+		return person;			
 	}
 
 
